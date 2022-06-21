af11c1d8a280f10ba8e9d8852efda7bd98dff09b
STOKLOSA THEO <theo.stoklosa@etu.unistra.fr>
2022-06-21 17:11:30 +0200

    Modification du header

--- src/components/header.js
+++ src/components/header.js
@@ -2,40 +2,13 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
-      display: `flex`,
-      alignItems: `center`,
-      justifyContent: `space-between`,
-    }}
-  >
-    <Link
-      to="/"
-      style={{
-        fontSize: `var(--font-sm)`,
-        textDecoration: `none`,
-      }}
-    >
-      {siteTitle}
-    </Link>
-    <img
-      alt="Gatsby logo"
-      height={20}
-      style={{ margin: 0 }}
-      src="https://img1.freepng.fr/20180706/guh/kisspng-logo-xunit-random-org-randomness-computer-software-logo-github-5b3fe6a8538de1.5691156615309144723423.jpg"
-    />
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+function Header() {
+  return (
+    <div>
+      <img src="https://crucialconversations.axelperformance.com/images/logo.svg" alt="Logo Axel Performance" />
+      <button>Prendre rendez-vous</button>
+    </div>
+  )
 }
 
 export default Header
